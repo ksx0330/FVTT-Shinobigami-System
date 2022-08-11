820750222873874de49fc26a087c999678cfc084
LTaeng <ksx0330@naver.com>
2022-08-12 01:01:19 +0900

    bug fix

--- module/talent-table.js
+++ module/talent-table.js
@@ -4,7 +4,7 @@ export class TalentTableForm extends FormApplication {
     static get defaultOptions() {
         return mergeObject(super.defaultOptions, {
             title: 'talent setting',
-            template: "systems/insane/templates/talent-table-form.html",
+            template: "systems/shinobigami/templates/talent-table-form.html",
             width: 500,
             closeOnSubmit: true,
             classes: ["shinobigami", "sheet"]
